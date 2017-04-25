2678e20d2fa685f6610930e9fa9c32844d4e9a24
alsoraimi <alsoraimi46@gmail.com>
2017-04-25 14:20:34 -0400

    completed constructors and added override for toString

--- Lab11/Person.cs
+++ Lab11/Person.cs
@@ -8,5 +8,59 @@ namespace Lab11
 {
     class Person
     {
+        private string Name;
+        private string Adress;
+
+
+        public string PName
+        {
+            get
+            {
+                return Name;
+            }
+
+            set
+            {
+                Name = value;
+            }
+
+        }
+
+        public string PAdress
+        {
+            get
+            {
+                return Adress;
+            }
+
+            set
+            {
+                Adress = value;
+            }
+        }
+
+        public Person ()
+        {
+            Name = "";
+            Adress = "";
+        }
+
+        public Person (string GetName, string GetAdress)
+        {
+            Name = GetName;
+            Adress = GetAdress;
+        }
+
+        public override string ToString()
+        {
+            return $"Person[Name ={Name}, Adress= {Adress}]";    //remember this shit its important
+
+
+        }
+
+
+
+
+
     }
 }
--- Lab11/Staff.cs
+++ Lab11/Staff.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace Lab11
 {
-    class Staff
+    class Staff:Person
     {
+        private string School;
+        private double Pay;
+
+        public string _School
+        {
+            get
+            {
+                return School;
+            }
+
+            set
+            {
+                School = value;
+            }
+        }
+
+        public double SPay
+        {
+            get
+            {
+                return Pay;
+            }
+
+            set
+            {
+                Pay = value;
+            }
+        }
+
+        public Staff ()
+        {
+            School = "";
+            Pay = 0;
+        }
+
+        public Staff (string GetSchool, double GetPay)
+        {
+            School = GetSchool;
+            Pay = GetPay;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Staff[Student[Person[Name ={PName}, Adress= {PAdress}], school = {School}, pay = {Pay}]";
+
+        }
     }
 }
